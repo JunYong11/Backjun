3281e9757dcfdd5ae4650f977f71df880238e05f
JunYong11 <wnsdyd1375@naver.com>
2023-04-15 18:54:50 +0900

    commit-2

--- src/st_1966.java
+++ src/st_1966.java
@@ -1,26 +1,27 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 public class st_1966 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Scanner sc = new Scanner(System.in);
-		Queue<Integer> qu = new LinkedList<>();
+		LinkedList<int []> list = new LinkedList<>();
 		
-		int test_case = sc.nextInt();
-		for(int i = 0;i<test_case;i++) {
-			int arr_num = sc.nextInt();
-			int num = sc.nextInt();
-			int arr [] = new int[arr_num];
+		int t = sc.nextInt();	//테스트 케이스
+		
+		while(t == 0) {
+			int N = sc.nextInt();	//문서갯수
+			int M = sc.nextInt();	//찾고자 하는 문서 위치
 			
-			for(int j= 0;j<arr_num;j++) {
-				
-
+			for(int i = 0;i<N;i++) {
+				list.add(new int[] { i, sc.nextInt() });
 			}
 			
-			
-			
-		}
+			int count = 0;
+			int [] front = list.poll();
+		} 
 	}
 
 }
