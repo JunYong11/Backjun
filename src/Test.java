c5d8dedb52bfbcc612e22ec15d5da0b85aa52eed
JunYong11 <92434317+JunYong11@users.noreply.github.com>
2023-04-18 13:26:12 +0900

    commit-2

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,4 @@
-import java.util.Deque;
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Scanner;
@@ -8,8 +8,51 @@ public class Test {
 
 	public static void main(String[] args)  {
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		System.out.println(num);
+		LinkedList<int []> list = new LinkedList<>();
+		
+		
+		int test = sc.nextInt();
+		
+		for(int i =0;i<test;i++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			
+			for(int j = 0;j<N;j++) {
+				list.add(new int[] {j,sc.nextInt()});
+			}
+			
+			while(!list.isEmpty()) {
+				int count = 0;
+				int [] front = list.poll();
+				boolean isMax = true;
+				
+				for(int j = 0;j<list.size();j++) {
+					if(front[1] < list.get(i)[1]) {
+						list.add(front);
+						
+						for(int a = 0;a<j;a++) {
+							list.add(list.poll());
+						}
+						isMax = false;
+						break;
+					}
+					
+					if(isMax = false) {
+						continue;
+					}
+								
+				}
+				count++;
+				if(front [0] == M) {
+					System.out.println(count);
+					list.clear();
+					test -= 1;
+					break;
+				}
+				
+			}
+		}
+
 		
 		
 		
--- src/st_1966.java
+++ src/st_1966.java
@@ -3,24 +3,55 @@ import java.util.Queue;
 import java.util.Scanner;
 public class st_1966 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		Scanner sc = new Scanner(System.in);
-		Queue<Integer> qu = new LinkedList<>();
+		LinkedList<int[]> list = new LinkedList<>();
 		
-		int test_case = sc.nextInt();
-		for(int i = 0;i<test_case;i++) {
-			int arr_num = sc.nextInt();
-			int num = sc.nextInt();
-			int arr [] = new int[arr_num];
+		int test = sc.nextInt();
+		
+		while(test != 0) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
 			
-			for(int j= 0;j<arr_num;j++) {
-				
-
+			for(int i =0;i<N;i++) {
+				list.add(new int[] {i,sc.nextInt()}	);
 			}
 			
+			int count = 0;
 			
+			while(!list.isEmpty()) {
+				int f_number [] = list.poll();	//첫번째 값 저장
+				boolean is_Max = true;
+				
+				for(int i = 0;i<list.size();i++) {
+					
+					if(f_number[1] < list.get(i)[1]) {
+						list.add(f_number);
+						
+						for(int j = 0;j<i;j++) {
+							list.add(list.poll());
+						}
+						
+						is_Max = false;
+						break;
+					}
+				}
+				
+				if(is_Max == false) {
+					continue;
+				}
+				
+				count++;
+				if(f_number[0] == M) {
+					System.out.println(count);
+					list.clear();
+					test -= 1;
+					break;
+				}
+			}
 			
 		}
+				
 	}
-
+	
 }
