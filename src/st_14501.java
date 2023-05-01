57e0ff29167cbcdf80e2b689ac48c50c7c074fff
JunYong11 <92434317+JunYong11@users.noreply.github.com>
2023-05-01 18:31:28 +0900

    2023-05-01-06:31
    d

--- src/st_14501.java
+++ src/st_14501.java
@@ -5,27 +5,7 @@ public class st_14501 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int day = sc.nextInt();	//입력받은 일수
 		
-		int [] time = new int[day];		//걸리는 시
-		int [] money = new int[day];	//받는돈
-		
-		int [] result = new int[day];	//결과값 저장
-		
-		for(int i = 0;i<day;i++) {
-			time[i] = sc.nextInt();
-			money[i] = sc.nextInt();
-			
-			
-		}
-		
-		for(int i = 0;i<day;i++) {
-			if(i+time[i] <=day) {
-				result[i+time[i]] = Math.max(result[i+time[i]], result[i]+money[i]);			
-				}
-			
-			
-		}
 		
 		
 	}
