11135e424608a43dbe24bed14014f86d5c6f9280
JunYong11 <92434317+JunYong11@users.noreply.github.com>
2023-07-23 14:00:06 +0900

    2023-07-23
    d

--- src/Test.java
+++ src/Test.java
@@ -1,53 +1,21 @@
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Test {
-	/**
-	 * 백준 15486 퇴사2 (https://www.acmicpc.net/problem/15486)
-	 */
+
 	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) {
 		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String s ="wsadasd";
+		String [] Arrs = s.split("");
 		
-		int n = Integer.parseInt(reader.readLine());	//일수 입력 변
-		
-		int[] t = new int[n];	//작업하는데 걸리는 시
-		int[] p = new int[n];	//완료 보상
-		
-		StringTokenizer st;
-		for (int i=0; i<n; i++) {
-			st = new StringTokenizer(reader.readLine());
+		Arrs.equals("p");
+		for(int i =0;i<s.length();i++) {
 			
-			t[i] = Integer.parseInt(st.nextToken());	//걸리는 일수 입
-			p[i] = Integer.parseInt(st.nextToken());	//보상금 입
 		}
-		
-		//dp : N일에 얻을 수 있는 최대 수익
-		int[] dp = new int[n+1];
-		
-		//점화식
-		//현재 날짜에서 소요 시간과 비용을 더해 dp에 저장한다.
-		//이후, 중복될 때 최대값을 넣는다.
-		//dp[i + t[i]] = max(dp[i + t[i]], dp[i] + p[i]);
-		
-		for (int i=0; i<n; i++) {	//일수만큼 반복
-			if (i + t[i] <= n) {
-				//날짜가 범위를 넘어가지 않는 경우
-				dp[i + t[i]] = Math.max(dp[i + t[i]], dp[i] + p[i]);
-			}
-			//현재 경우의 수가 0일 수 있기 때문에 이전의 최대값을 넣어줌.
-			//해당 날짜에 일할 수 없다면, 이전까지 일한 최대 수당을 넣어주어야 한다.
-			dp[i+1] = Math.max(dp[i+1], dp[i]);
-		}
-		System.out.println(dp[n]);
-		
-	}//main
-}//Resign2
+	}
+}
 
 
 
--- src/st_1324.java
+++ src/st_1324.java
@@ -4,68 +4,14 @@ public class st_1324 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
-		int [] arr1 = new int[t];
-		int [] arr2 = new int[t];
-		int temp = 0;
-		int count = 0;
+		int arr1[] = new int[t];
+		int arr2[] = new int[t];
 		
-		for(int i = 0;i<t;i++) {
-			arr1[i] = sc.nextInt();
-		}
-	
-		for(int i = 0;i<t;i++) {
-			arr2[i] = sc.nextInt();
-		}
+		int dp[] = new int[t+1];
 		
-		for(int i = 1;i<t;i++) {
-			for(int j=i;j<t;j++) {
-				if(arr1[i-1] <= arr1[j]) {
-					temp = arr1[i];
-					arr1[i] = arr1[j];
-					arr1[j] = temp;
-					break;
-				}
-			}
+		for(int i = 0;i<=t;i++) {
+			
 		}
-		
-		for(int i = 1;i<t;i++) {
-			for(int j=i;j<t;j++) {
-				if(arr2[i-1] <= arr2[j]) {
-					temp = arr2[i];
-					arr2[i] = arr2[j];
-					arr2[j] = temp;
-					break;
-				}
-			}
-		}
-		
-		for(int i =1;i<t;i++) {
-			if(arr1[i-1]>arr1[i]||arr1[i-1] == 0) {
-				arr1[i]=0;
-			}
-		}
-		
-		for(int i =1;i<t;i++) {
-			if(arr2[i-1]>arr2[i]||arr2[i-1] == 0) {
-				arr2[i]=0;
-			}
-		}
-		
-		for(int i = 0;i<t;i++) {
-			for(int j = 0;j<t;j++) {
-				if(arr2[i] == arr1[j] && arr2[i] != 0) {
-					count++;
-					arr1[j] = 0;
-					break;
-				}
-				else if(arr2[i] == 0) {
-					break;
-				}
-				else continue;
-				
-			}
-		}
-		System.out.println(count);
 		
 	}
 	
