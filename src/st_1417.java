33153de127078229bb37c9bd955ba96bcae8c8b8
JunYong11 <92434317+JunYong11@users.noreply.github.com>
2023-08-22 16:35:26 +0900

    2023-08-22
    d

--- src/st_1417.java
+++ src/st_1417.java
@@ -1,23 +1,43 @@
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
+
 
 public class st_1417 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int dasom = 0;
-		int temp = 0;
-		int j = 1;
 		int num = sc.nextInt();
-		int [] hu = new int[num];
+		Integer [] hu = new Integer[num];
 		for(int i =0;i<hu.length;i++) {
 			hu[i] = sc.nextInt();
 		}
-		vote(num,hu);
 		/*
-		dasom = hu[0];
+		for(int i =0 ;i<hu.length;i++) {
+			System.out.println(hu[i]);
+		}
+		System.out.println("------------------------------------");
+		
+		Arrays.sort(hu,1,num, Comparator.reverseOrder());
+		
+		for(int i =0 ;i<hu.length;i++) {
+			System.out.println(hu[i]);
+		}
+		*/
+		
+		
+		if(num != 1) {
+			vote3(num,hu);
+		}
+		else {
+			System.out.println(0);
+		}
+		
+	}
+	
+	
+	static void vote1(int num, Integer[] hu) {
+		int dasom = hu[0];
+		int j = 0;
+		int temp = 0;
 		Arrays.sort(hu);
 		while(true) {
 			if(num == 1) {
@@ -45,13 +65,12 @@ public class st_1417 {
 		}
 		
 		System.out.println(dasom - hu[0]);
-		*/
-		
 	}
 	
-	static int vote(int num, int[] hu) {
+	static void vote2(int num, Integer[] hu) {
+
 		int dasom = hu[0];
-		int [] hu1 = new int [num-1];
+		Integer[] hu1 = new Integer[num - 1];
 		
 		for(int i =0;i<hu1.length;i++) {
 			hu1[i] = hu[i+1];
@@ -59,19 +78,42 @@ public class st_1417 {
 		
 		while(true) {
 			int j = 0;
-			Arrays.sort(hu1,Comparator.reverseOrder());
+			Arrays.sort(hu1, Comparator.reverseOrder());
 			if(dasom <= hu1[j]) {
 				dasom +=1;
 				hu1[j] -=1;
 			}
 			else if(dasom > hu1[j]) {
-				System.out.println(dasom);
-				return dasom;
+				System.out.println(dasom-hu[0]);
+				break;
 				
 			}
 		}
 	}
 
+	static void vote3(int num, Integer[] hu) {
+		int dasom = hu[0];
+		
+		while(true) {
+			int j = 1;
+			Arrays.sort(hu,1,num, Comparator.reverseOrder());
+			for(int i =1;i<hu.length;i++) {
+				System.out.println(hu[i]);
+			}
+			System.out.println("");
+			System.out.println("-------------------------------------");
+			
+			if(dasom <= hu[j]) {
+				dasom +=1;
+				hu[j] -=1;
+			}
+			else if(dasom > hu[j]) {
+				System.out.println(dasom-hu[0]);
+				break;
+				
+			}
+		}
+	}
 }
 
 
