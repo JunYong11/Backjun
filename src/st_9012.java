53067071d76ed3f22a397da5ab6661ad249ab5f4
JunYong11 <92434317+JunYong11@users.noreply.github.com>
2023-12-13 16:01:53 +0900

    st_9012

--- src/st_9012.java
+++ src/st_9012.java
@@ -1,9 +1,55 @@
-
+import java.util.Scanner;
 public class st_9012 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int Lnum = 0;
+		int Rnum = 0;
+		
+		int number = sc.nextInt();
+		String arr[] = new String [number];
+		
+		for(int i = 0;i<number;i++) {
+			arr[i] = sc.next();
+		}
+		
+		for(int i =0;i<number;i++) {
+			String temp = arr[i];
+			String [] c = temp.split("");
+			int check = 0;
+			if(c.length % 2 ==0) {
+				for(int j =0;j<c.length;j++) {
+					if(c[j].contains("(")) {
+						Lnum ++;
+					}
+					else {
+						Rnum++;
+						
+						if(Rnum >Lnum) {
+							System.out.println("NO");
+							check = 1;
+							Lnum = 0;
+							Rnum = 0;
+							break;
+						}
+					}
+				}
+				if(check == 0 && Lnum == Rnum) {
+					System.out.println("YES");
+					Lnum = 0;
+					Rnum = 0;
+				}
+				else if(check == 0) {
+					System.out.println("NO");
+					Lnum = 0;
+					Rnum = 0;
+				}	
+			}
+			else {
+				System.out.println("NO");
+				continue;
+			}
+		}
 	}
-
 }
